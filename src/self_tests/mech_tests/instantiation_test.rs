e4b4bf1d8a1a34adfae056f15203d0a671bcc1e6
frakkekko93 <dermefrancesco1@gmail.com>
2023-03-29 16:01:14 +0200

    Fixed mech inst. tests also for CTR.

--- src/self_tests/mech_tests/instantiation_test.rs
+++ src/self_tests/mech_tests/instantiation_test.rs
@@ -1,8 +1,11 @@
+use crate::mechs::ctr_mech::CtrDrbgMech;
 use crate::mechs::gen_mech::DRBG_Mechanism_Functions;
 use crate::mechs::hash_mech::HashDrbgMech;
 use crate::mechs::hmac_mech::HmacDrbgMech;
+use rand::Rng;
 use sha2::*;
 use crate::self_tests::formats::*;
+use des::*;
 
 const AL_NAME: &str = "MECH-TESTS::instantiation_test";
 
@@ -27,13 +30,28 @@ pub fn run_tests<T: DRBG_Mechanism_Functions>() -> usize {
                 test_empty_nonce::<T>();
     }
     else {
-        return 0;
+        return norm_op::<T>() +
+                test_fun_not_approved::<CtrDrbgMech<TdesEde2>>("TdesEde2") + 
+                test_fun_not_approved::<CtrDrbgMech<TdesEde3>>("TdesEde3") + 
+                test_fun_not_approved::<CtrDrbgMech<TdesEee2>>("TdesEee2") +
+                test_fun_not_approved::<CtrDrbgMech<TdesEee3>>("TdesEee3") +
+                test_empty_entropy::<T>() +
+                test_empty_nonce::<T>();
     }
 }
 
 /*  Testing normal instantiation of the mechanism. */
 fn norm_op<T: DRBG_Mechanism_Functions>() -> usize{
-    let res = T::new("Trail entropy".as_bytes(), "Trial nonce".as_bytes(), "Trial pers".as_bytes(), &mut 128);
+    let mut entropy = Vec::<u8>::new();
+    let entropy_part: [u8; 32] = rand::thread_rng().gen();
+    entropy.append(&mut entropy_part.to_vec());
+
+    if T::drbg_name() == "CTR-DRBG" {
+        let entropy_part2: [u8; 16] = rand::thread_rng().gen();
+        entropy.append(&mut entropy_part2.to_vec());
+    }
+
+    let res = T::new(&entropy, "Trial nonce".as_bytes(), "Trial pers".as_bytes(), &mut 128);
 
     if check_res(res.is_none(), false, 
             "norm_op".to_string(), 
@@ -47,7 +65,16 @@ fn norm_op<T: DRBG_Mechanism_Functions>() -> usize{
 
 /*  Testing use of unapproved functions. */
 fn test_fun_not_approved<T: DRBG_Mechanism_Functions>(fun_id: &str) -> usize{
-    let res = T::new("Trail entropy".as_bytes(), "Trial nonce".as_bytes(), "Trial pers".as_bytes(), &mut 128);
+    let mut entropy = Vec::<u8>::new();
+    let entropy_part: [u8; 32] = rand::thread_rng().gen();
+    entropy.append(&mut entropy_part.to_vec());
+
+    if T::drbg_name() == "CTR-DRBG" {
+        let entropy_part2: [u8; 16] = rand::thread_rng().gen();
+        entropy.append(&mut entropy_part2.to_vec());
+    }
+
+    let res = T::new(&entropy, "Trial nonce".as_bytes(), "Trial pers".as_bytes(), &mut 128);
 
     let mut test_name = "test_fun_not_approved::".to_string();
     test_name.push_str(fun_id);
@@ -87,14 +114,35 @@ fn test_empty_entropy<T: DRBG_Mechanism_Functions>() -> usize {
 
 /*  Testing that instantiation with empty nonce fails */
 fn test_empty_nonce<T: DRBG_Mechanism_Functions>() -> usize {
-    let res = T::new("Trial entropy".as_bytes(), "".as_bytes(), "Trial pers".as_bytes(), &mut 128);
+    let mut entropy = Vec::<u8>::new();
+    let entropy_part: [u8; 32] = rand::thread_rng().gen();
+    entropy.append(&mut entropy_part.to_vec());
 
-    if check_res(res.is_none(), true, 
-            "test_empty_nonce".to_string(), 
-            AL_NAME.to_string(), 
-            "instantiation with empty nonce of DRBG mechanism succeeded.".to_string(), 
-            "instantiation with empty nonce of DRBG mechanism failed, as expected.".to_string()) != 0{
-        return 1;
+    if T::drbg_name() == "CTR-DRBG" {
+        let entropy_part2: [u8; 16] = rand::thread_rng().gen();
+        entropy.append(&mut entropy_part2.to_vec());
     }
+
+    let res = T::new(&entropy, "".as_bytes(), "Trial pers".as_bytes(), &mut 128);
+
+    if T::drbg_name() != "CTR-DRBG" {
+        if check_res(res.is_none(), true, 
+                "test_empty_nonce".to_string(), 
+                AL_NAME.to_string(), 
+                "instantiation with empty nonce of DRBG mechanism succeeded.".to_string(), 
+                "instantiation with empty nonce of DRBG mechanism failed, as expected.".to_string()) != 0{
+            return 1;
+        }
+    }
+    else {
+        if check_res(res.is_none(), false, 
+                "test_empty_nonce".to_string(), 
+                AL_NAME.to_string(), 
+                "instantiation with empty nonce of DRBG mechanism failed (empty nonce allowed with CTR with no DF).".to_string(), 
+                "instantiation with empty nonce of DRBG mechanism succeeded, as expected (empty nonce allowed with CTR with no DF).".to_string()) != 0{
+            return 1;
+        }
+    }
+
     0
 }
