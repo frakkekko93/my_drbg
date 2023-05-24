b91d5bb71a8affb5f89260dd90c4ec2df6f3eae8
frakkekko93 <dermefrancesco1@gmail.com>
2023-05-24 16:37:43 +0200

    Revert "Final Version 1.0 - 2023-05-24"
    This reverts commit 20b9fcc169fc6641d4c51d77a0e8b8bfc3ea01d5.
    
    Annullament commit per modifiche aggiuntive da apportare.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 extern crate rust_nist_drbg;
 
 fn main(){  
-    // let res = rust_nist_drbg::self_tests::run_tests::run_all();
-    // if  res > 0
-    // {
-    //     panic!("MAIN: FATAL ERROR - {res} self-tests have failed, plese check testing log from more information.")
-    // }
+    let res = rust_nist_drbg::self_tests::run_tests::run_all();
+    if  res > 0
+    {
+        panic!("MAIN: FATAL ERROR - {res} self-tests have failed, plese check testing log from more information.")
+    }
 
     rust_nist_drbg::demos::run_demo();
 }
